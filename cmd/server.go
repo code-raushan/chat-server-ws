dc194d35568003585d1262a36a26d85224d2b924
Raushan Kumar <raushan.development@gmail.com>
2024-08-01 22:41:44 +0530

    feat: redis connection established

--- cmd/server.go
+++ cmd/server.go
@@ -1,3 +1,39 @@
 package main
 
-func main(){}
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/websocket"
+	"github.com/joho/godotenv"
+	"github.com/redis/go-redis/v9"
+)
+
+var(
+	redisClient *redis.Client
+	upgrader = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+)
+func main(){
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	redisURI := os.Getenv("REDIS_URI")
+
+	if redisURI == "" {
+		log.Fatal("Redis URI cannot be empty")
+	}
+
+	redisClient = redis.NewClient(&redis.Options{
+		Addr: redisURI,
+	})
+
+	defer redisClient.Close()
+}
